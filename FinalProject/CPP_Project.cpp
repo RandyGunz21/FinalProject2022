7a5f9667b8ac35ddc429da4eac0711a6bce3500e
RandyGunz21 <116243144+RandyGunz21@users.noreply.github.com>
2022-10-31 14:29:44 +0700

    Update CPP_Project.cpp

--- FinalProject/CPP_Project.cpp
+++ FinalProject/CPP_Project.cpp
@@ -20,6 +20,8 @@ int chosing(); int chosing2();
 int** addition(int **arr1, int **arr2, int *row, int *col);
 int** substraction(int **arr1, int **arr2, int *row, int *col);
 int** multiply(int **arr1, int **arr2, int *resRow, int *resCol, int *sameRowCol);
+int** scalar(int **arr, int *row, int *col, int *k);
+int** transpose(int **arr, int *row, int *col);
 
 int main(){
     main:
@@ -188,6 +190,101 @@ int main(){
             break;
         }
         break;
+
+        case 4:
+        system("cls");
+        menuHeader(4); note(4);
+        switch(chosing()){
+            case 1:
+            scalar:
+            int mul;
+            system("cls"); menuHeader(4);
+            note(4); inputHeader(&ordo1.row, &ordo1.column);
+            m1 = arrDec(&ordo1.row, &ordo1.column);
+            cout << "Matrix:\n"; inputMatrix(m1, &ordo1.row, &ordo1.column);
+            cout << "\nMultiplier: "; cin >> mul;
+            cout << GREEN << "\nResult:\n";
+            printMatrix(scalar(m1, &ordo1.row, &ordo1.column, &mul),
+            &ordo1.row, &ordo1.column); cout << WHITE;
+            switch(chosing2()){
+                case 1:
+                goto scalar;
+
+                case 2:
+                goto main;
+
+                case 3:
+                system("exit");
+            }
+            break;
+
+            case 2:
+            goto main;
+            break;
+        }
+        break;
+
+        case 5:
+        system("cls");
+        menuHeader(5); note(5);
+        switch(chosing()){
+            case 1:
+            trans:
+            system("cls"); menuHeader(5);
+            note(5); inputHeader(&ordo1.row, &ordo1.column);
+            m1 = arrDec(&ordo1.row, &ordo1.column);
+            cout << "Matrix:\n"; inputMatrix(m1, &ordo1.row, &ordo1.column);
+            cout << GREEN << "\nResult:\n";
+            printMatrix(transpose(m1, &ordo1.row, &ordo1.column),
+            &ordo1.column, &ordo1.row); cout << WHITE;
+            switch(chosing2()){
+                case 1:
+                goto trans;
+
+                case 2:
+                goto main;
+
+                case 3:
+                system("exit");
+            }
+            break;
+
+            case 2:
+            goto main;
+            break;
+        }
+        break;
+
+        case 6:
+        system("cls");
+        menuHeader(6); note(6);
+        switch(chosing()){
+            case 1:
+            determinant:
+            system("cls"); menuHeader(6);
+            note(6); inputHeader(&ordo1.row, &ordo1.column);
+            m1 = arrDec(&ordo1.row, &ordo1.column);
+            cout << "Matrix:\n"; inputMatrix(m1, &ordo1.row, &ordo1.column);
+            cout << GREEN << "\nResult:\n";
+            printMatrix(transpose(m1, &ordo1.row, &ordo1.column),
+            &ordo1.column, &ordo1.row); cout << WHITE;
+            switch(chosing2()){
+                case 1:
+                goto determinant;
+
+                case 2:
+                goto main;
+
+                case 3:
+                system("exit");
+            }
+            break;
+
+            case 2:
+            goto main;
+            break;
+        }
+        break;
     }
     return 0;
 }
@@ -210,10 +307,10 @@ int mainMenu(){
     cout << "Please choose the matrix operation you want\n";
     cout << "1. Matrix Addition\n" << "2. Matrix Substraction\n";
     cout << "3. Matrix Multiplication\n" << "4. Scalar Multiplication\n";
-    cout << "5. Transpose\n" << "6. Inverse\n";
-    cout << "7. Determinant\n" << "8. Exit\n";
+    cout << "5. Transpose\n" << "6. Daterminant\n";
+    cout << "7. Inverse\n" << "8. Exit\n";
     do{
-        cout << "Choose[1-7] >> ";
+        cout << "Choose[1-8] >> ";
         cin >> n;
     }
     while(n > 8 || n < 1);
@@ -268,6 +365,24 @@ void menuHeader(int n){
         cout << "|        MATRIX MULTIPLICATION       |" << endl;
         cout << "======================================" << endl;
         break;
+
+        case 4:
+        cout << "======================================" << endl;
+        cout << "|        SCALAR MULTIPLICATION       |" << endl;
+        cout << "======================================" << endl;
+        break;
+
+        case 5:
+        cout << "======================================" << endl;
+        cout << "|          TRANSPOSE MATRIX          |" << endl;
+        cout << "======================================" << endl;
+        break;
+
+        case 6:
+        cout << "======================================" << endl;
+        cout << "|         MATRIX DETERMINANT         |" << endl;
+        cout << "======================================" << endl;
+        break;
     }
 }
 
@@ -290,8 +405,26 @@ void note(int n){
         break;
 
         case 3:
+        cout << "Note:\n";
         cout << "The product of two matrices A and B is defined if the number"
         << " of columns of A is equal to the number of rows of B.\n";
+        break;
+
+        case 4:
+        cout << "Note:\n";
+        cout << "The term scalar multiplication refers to the product of a real number "
+        << "and a matrix.\nIn scalar multiplication, each entry in the matrix is multiplied "
+        << "by the given scalar.\n";
+        break;
+
+        case 5:
+        cout << "Note:\n";
+        cout << "The transpose of a matrix is found by interchanging its rows into columns or columns into rows.\n";
+        break;
+
+        case 6:
+        cout << "Note:\n";
+        cout << "Determinant of matrix only available for square matrix such as 2x2 or 3x3 matrix.\n";
         break;
     }
 }
@@ -353,3 +486,31 @@ int** multiply(int **arr1, int **arr2, int *resRow, int *resCol, int *sameRowCol
     }
     return result;
 }
+
+int** scalar(int **arr, int *row, int *col, int *k){
+    int **result;
+    result = arrDec(row, col);
+    for(int i = 0; i < *row; i++){
+        for(int j = 0; j < *col; j++){
+            result[i][j] = arr[i][j]*(*k);
+        }
+    }
+    return result;
+}
+
+int** transpose(int **arr, int *row, int *col){
+    int** result;
+    result = arrDec(col, row);
+    for(int i = 0; i < *row; i++){
+        for(int j = 0; j < *col; j++){
+            result[j][i] = arr[i][j];
+        }
+    }
+    return result;
+}
+
+/*int** det(int **arr, int *row, int *col){
+    int** result;
+    result = arrDec(row, column);
+    
+}*/
